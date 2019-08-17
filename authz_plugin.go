6a1aa780c5394a60f413a90817f97a2758a8b5bd
tharindulak <tharindu.jayathilake1@gmail.com>
2019-08-17 13:14:01 +0530

    Change log to fmt

--- authz_plugin.go
+++ authz_plugin.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cesanta/docker_auth/auth_server/authz"
-	"log"
 )
 
 type authorizer string
 
 func (a authorizer) Authorize(ai *authz.AuthRequestInfo) ([]string, error) {
-	log.Printf("Received auth request info: %v", ai)
+	fmt.Printf("Received auth request info: %v", ai)
 	return ai.Actions, nil
 }
 
